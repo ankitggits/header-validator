02a33777a46374ba52c0e62410e1e732c2254cb3
ankit <ankit.singh@storebrand.no>
2018-05-28 10:10:36 +0200

    sample commit on dev

--- src/main/java/com/dependentlearners/utility/HeaderValidatorApplication.java
+++ src/main/java/com/dependentlearners/utility/HeaderValidatorApplication.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HeaderValidatorApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Runs with default port 8080");
 		SpringApplication.run(HeaderValidatorApplication.class, args);
 	}
 
