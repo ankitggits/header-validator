7b7ce7e94a031cf3969a4461b9c1a1686a594c07
ankit <ankit.singh@storebrand.no>
2018-05-28 10:17:33 +0200

    error log

--- src/main/java/com/dependentlearners/utility/component/ValidatedHeaderHandlerMethodArgumentResolver.java
+++ src/main/java/com/dependentlearners/utility/component/ValidatedHeaderHandlerMethodArgumentResolver.java
@@ -1,12 +1,14 @@
 package com.dependentlearners.utility.component;
 
 import com.dependentlearners.utility.annotation.ValidatedHeader;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 public class ValidatedHeaderHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
 	String headerName;
@@ -28,6 +30,7 @@ public class ValidatedHeaderHandlerMethodArgumentResolver implements HandlerMeth
 			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 		String headerValue = webRequest.getHeader(headerName);
 		if(!headerValue.matches(regex)){
+			log.error("header does not match");
 			throw new RuntimeException(headerName+" -> Header not matched");
 		}
 		return headerValue;
